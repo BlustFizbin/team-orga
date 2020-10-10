97b8ed0a3f41fadc9c92b3987f9c6113e1f54ee1
Sophia Donderer <sophia@studio-fizbin.de>
2020-10-10 13:13:26 +0200

    added MainScene & script PlayerBehaviour

---
+++ Assets/PlayerBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [SerializeField] private float speedX = 2;
+    [SerializeField] private float speedY = 1;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+
+        if (inputX!= 0)
+        {
+            if(inputX > 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            } else transform.localScale = new Vector3(-1, 1, 1);
+
+            Vector3 currentPos = transform.position;
+            currentPos.x = currentPos.x + speedX * inputX;
+            transform.position = currentPos;
+        }
+
+        float inputY = Input.GetAxis("Vertical");
+
+        if (inputY != 0)
+        {
+            Vector3 currentPos = transform.position;
+            currentPos.y = currentPos.y + speedY * inputY;
+            transform.position = currentPos;
+        }
+    }
+}
