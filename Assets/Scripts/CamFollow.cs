f3ed301ea742c6a106c663d14cc2ff30442ff543
Sophia Donderer <sophia@studio-fizbin.de>
2020-10-10 14:59:30 +0200

    cam follow

---
+++ Assets/Scripts/CamFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    private Transform player;
+    [SerializeField] private float smooth = 5;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = new Vector3(player.position.x, player.position.y, -10);
+        transform.position = Vector3.Lerp(transform.position, playerPos,Time.deltaTime*smooth);
+    }
+}
