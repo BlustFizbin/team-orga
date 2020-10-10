42a3ae527463c2cf44088dbd4cdd6c0b5fbc0150
Sophia Donderer <sophia@studio-fizbin.de>
2020-10-10 14:43:55 +0200

    updated player controls

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -6,10 +6,11 @@ public class PlayerBehaviour : MonoBehaviour
 {
     [SerializeField] private float speedX = 2;
     [SerializeField] private float speedY = 1;
+    private Rigidbody2D rigid;
 
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -23,18 +24,20 @@ public class PlayerBehaviour : MonoBehaviour
                 transform.localScale = new Vector3(-1, 1, 1);
             } else transform.localScale = new Vector3(1, 1, 1);
 
-            Vector3 currentPos = transform.position;
+            rigid.AddForce(new Vector2(inputX*speedX*Time.deltaTime, 0), ForceMode2D.Impulse);
+            /*Vector3 currentPos = transform.position;
             currentPos.x = currentPos.x + speedX * inputX;
-            transform.position = currentPos;
+            transform.position = currentPos;*/
         }
 
         float inputY = Input.GetAxis("Vertical");
 
         if (inputY != 0)
         {
-            Vector3 currentPos = transform.position;
+            rigid.AddForce(new Vector2(0, inputY*speedY*Time.deltaTime), ForceMode2D.Impulse);
+            /*Vector3 currentPos = transform.position;
             currentPos.y = currentPos.y + speedY * inputY;
-            transform.position = currentPos;
+            transform.position = currentPos;*/
         }
     }
 }
