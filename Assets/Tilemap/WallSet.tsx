ac53f5b446fc9883377b6f30fabcee12dd871c23
Kasimir Blust <kasimir.blust@studio-fizbin.de>
2020-10-10 16:27:17 +0200

    new tilemap

---
+++ Assets/Tilemap/WallSet.tsx
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="WallSet" tilewidth="32" tileheight="32" tilecount="10" columns="5">
+ <image source="../Sprites/Env/env_wall_01.png" trans="000000" width="160" height="64"/>
+ <terraintypes>
+  <terrain name="Wall" tile="2"/>
+  <terrain name="Transparent" tile="2"/>
+ </terraintypes>
+ <tile id="0" terrain="1,0,1,0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32.1818" y="-0.727273">
+    <polygon points="0,0 -15.7273,-0.0909091 -19.9091,33.0909 1.54545,32.9091"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1" terrain="0,1,1,1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="17" y="-1.36364">
+    <polygon points="0,0 0.363636,7.45455 -4.54545,10.1818 -17.8182,9.63636 -18.3636,-0.272727"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2" terrain="0,0,0,0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.909091" y="-0.727273" width="33.6364" height="33.2727"/>
+  </objectgroup>
+ </tile>
+ <tile id="3" terrain="1,1,0,0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-1.72727" y="22.4545">
+    <polygon points="0,0 35.1818,0 35.6364,11.2727 0.363636,10.3636"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4" terrain="1,1,0,1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-2.54545" y="22.1818">
+    <polygon points="0,0 17.1818,4.27273 20.7273,10.0909 1,11.2727"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5" terrain="0,0,1,1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-1.18182" y="7.81818">
+    <polygon points="0,0 12.5455,1.81818 19.9091,1.90909 23.9091,0.545455 34.4545,0.727273 34.7273,-9.36364 -1,-9.63636"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6" terrain="1,0,1,1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="12" y="-2">
+    <polygon points="0,0 1.18182,6.18182 6.63636,9.63636 20.7273,10.0909 20.6364,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="7" terrain="0,1,0,1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16.7273" y="-2">
+    <polygon points="0,0 0.636364,35.7273 -17.3636,34.9091 -18.2727,-0.0909091"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="8" terrain="1,1,1,0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="18.2727" y="33.6364">
+    <polygon points="0,0 1.36364,-8.90909 14,-13.3636 15,-0.727273"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="9" terrain="1,1,1,1"/>
+</tileset>
--- Assets/Tilemap/gvdfgdfgdfg.tsx
+++ Assets/Tilemap/gvdfgdfgdfg.tsx
@@ -1,68 +1,76 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="BlackAndWhite" tilewidth="32" tileheight="32" tilecount="12" columns="4">
- <image source="pixil-frame-0(2).png" width="128" height="96"/>
+<tileset version="1.4" tiledversion="1.4.2" name="BlackAndWhite" tilewidth="32" tileheight="32" tilecount="104" columns="13">
+ <image source="pixil-frame-0(2).png" width="440" height="274"/>
  <terraintypes>
-  <terrain name="Black" tile="5"/>
-  <terrain name="White" tile="11"/>
+  <terrain name="Green Bottom" tile="3"/>
+  <terrain name="Blueish Bottom" tile="14"/>
  </terraintypes>
- <tile id="0" terrain="1,1,1,0">
+ <tile id="0" terrain="0,0,0,1"/>
+ <tile id="1" terrain="0,0,1,1"/>
+ <tile id="2" terrain="0,0,1,0"/>
+ <tile id="3" terrain="0,0,0,0"/>
+ <tile id="13" terrain="0,1,0,1"/>
+ <tile id="14" terrain="1,1,1,1"/>
+ <tile id="15" terrain="1,0,1,0"/>
+ <tile id="16" terrain="1,1,1,0"/>
+ <tile id="17" terrain="1,1,0,1"/>
+ <tile id="26" terrain="0,1,0,0"/>
+ <tile id="27" terrain="1,1,0,0"/>
+ <tile id="28" terrain="1,0,0,0"/>
+ <tile id="29" terrain="1,0,1,1"/>
+ <tile id="30" terrain="0,1,1,1"/>
+ <tile id="39">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="32.4545" y="5.81818">
-    <polygon points="0,0 -12.3636,0.818182 -25.4545,11.3636 -25.4545,28.0909 2,27.5455"/>
+   <object id="1" x="32.7273" y="8.27273">
+    <polygon points="0,0 -11.0909,-0.454545 -14.4545,1.54545 -21.4545,1.72727 -26.3636,0.272727 -33.3636,0.454545 -33.6364,-8.63636 0.272727,-8.45455"/>
    </object>
   </objectgroup>
  </tile>
- <tile id="1" terrain="1,1,0,0">
+ <tile id="40">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="6" width="32.2727" height="26.3636"/>
-  </objectgroup>
- </tile>
- <tile id="2" terrain="1,1,0,1">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="-0.909091" y="7">
-    <polygon points="0,0 6.81818,0.0909091 14.6364,7.81818 16,24.7273 11.0909,31.1818 -1.63636,26.9091"/>
+   <object id="1" x="18" y="0.181818">
+    <polygon points="0,0 -5.27273,31.5455 -4.27273,33.7273 15,33.4545 14.8182,-1.45455 1.90909,-1.54545"/>
    </object>
   </objectgroup>
  </tile>
- <tile id="4" terrain="1,0,1,0">
+ <tile id="41">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="6.90909" y="-0.272727">
-    <polygon points="0,0 0.454545,32.7273 26.8182,33 25.8182,-1.63636"/>
+   <object id="1" x="12.2727" y="-0.272727">
+    <polygon points="0,0 1.63636,6.81818 6.45455,8.09091 20,8.27273 20.1818,-0.272727"/>
    </object>
   </objectgroup>
  </tile>
- <tile id="5" terrain="0,0,0,0">
+ <tile id="43">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0.181818" y="0" width="31.9091" height="32"/>
+   <object id="1" x="0" y="0" width="32" height="32"/>
   </objectgroup>
  </tile>
- <tile id="6" terrain="0,1,0,1">
+ <tile id="52">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="-3.45455" y="-1.81818">
-    <polygon points="0,0 18.1818,1.90909 18.5455,35.0909 2.36364,32.8182"/>
+   <object id="1" x="16.9091" y="-0.818182">
+    <polygon points="0,0 0.545455,34.0909 -17.0909,33.4545 -17.0909,-0.727273"/>
    </object>
   </objectgroup>
  </tile>
- <tile id="8" terrain="1,0,1,1">
+ <tile id="53">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="7.09091" y="-0.818182">
-    <polygon points="0,0 0,2.90909 4.09091,16.2727 15.7273,20.8182 26.2727,20.9091 25.7273,0.727273"/>
+   <object id="1" x="-2.09091" y="22.2727">
+    <polygon points="0,0 35.9091,0.0909091 35.9091,10.6364 1.72727,10.2727"/>
    </object>
   </objectgroup>
  </tile>
- <tile id="9" terrain="0,0,1,1">
+ <tile id="54">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="-0.181818" y="20.0909">
-    <polygon points="0,0 29.3636,-0.363636 30.5455,-1.27273 33.0909,-1.09091 33.3636,-22.0909 -1.18182,-21.6364"/>
+   <object id="1" x="19.2727" y="32.2727">
+    <polygon points="0,0 0.545455,-6.27273 10.3636,-11.1818 13.7273,-10.4545 14.6364,1.09091"/>
    </object>
   </objectgroup>
  </tile>
- <tile id="10" terrain="0,1,1,1">
+ <tile id="55">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="14.7273" y="-0.727273">
-    <polygon points="0,0 0.363636,10.4545 -6.90909,19.6364 -15.5455,19.5455 -15.5455,-0.909091"/>
+   <object id="1" x="0.545455" y="22.2727">
+    <polygon points="0,0 14.3636,4.18182 17.1818,10.5455 -1.27273,10.2727 -2.27273,-0.727273"/>
    </object>
   </objectgroup>
  </tile>
- <tile id="11" terrain="1,1,1,1"/>
 </tileset>
